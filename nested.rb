d749c614f3e38533229daf3b40d441ca840b9304
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-14 18:56:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,14 +130,23 @@ end
 
 def adding_to_dennis
 	programmer_hash = 
-	{
-   :dennis_ritchie => {
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-  }
-	}  
+        }
+     }
+	  
 	programmer_hash[:dennis_ritchie][:languages] = "C", "Assembly"
   programmer_hash[:dennis_ritchie]
 
 
- end 
+ end  
